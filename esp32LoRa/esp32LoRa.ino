fa043fc21f13a1d060339784bbcc0027fe8a2bbc
Jonas Graubner <82174634+JoTec2002@users.noreply.github.com>
2023-10-30 15:53:18 +0100

    Initial Test

---
+++ esp32LoRa/esp32LoRa.ino
@@ -0,0 +1,13 @@
+#include <LoRa.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  // init LoRa
+  lorawan_setup();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
