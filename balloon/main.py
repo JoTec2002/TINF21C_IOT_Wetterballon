c9332aa37d29b16fb3f28d877827daee71cdad1a
Jonas Graubner <82174634+JoTec2002@users.noreply.github.com>
2023-11-01 20:38:12 +0100

    Add MPU Basic output

--- balloon/main.py
+++ balloon/main.py
@@ -8,17 +8,20 @@ license: MIT
 import time
 from loguru import logger
 from GPS.gps import Gps
+from MPU9050.mpu9050 import MPU9050
 
 
 class Main:
     def __init__(self):
-        logger.add("gps.log")
+        logger.add("Logs.log")
         self.GPS = Gps()
+        self.MPU9050 = MPU9050()
         logger.info("GPS init")
 
     def loop(self):
         while True:
             logger.info(self.GPS.read_location())
+            logger.info(self.MPU9050.read_position_data())
             time.sleep(20)
 
 if __name__ == "__main__":
