b6d90e43cf3bbf52152d2f5648ae32cc1481221d
Dana Frey <inf21099@lehre.dhbw-stuttgart.de>
2023-11-13 15:45:15 +0100

    Current state with houseform still contains errors

--- next/src/components/navigationbar.tsx
+++ next/src/components/navigationbar.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import { Form as HouseForm, Field } from "houseform";
 
 import { Avatar, Dropdown, Navbar } from 'flowbite-react';
 
 const Navigationbar = () => {
 
   const [dropdownItems, setDropdownItems] = useState<string[]>([]);
+  const [addingBalloon, setAddingBalloon] = useState(false);
 
-  const addDropdownItem = () => {
-    const newItem = "Balloon " + (dropdownItems.length + 1);
-    setDropdownItems(prevItems => [...prevItems, newItem]);
+  const setAddingBalloonTrue = () => {
+    setAddingBalloon(true);
   };
-  
+
+  const addBalloon = (data: any) => {
+    if (data.balloonname.trim() !== '') {
+      setDropdownItems(prevItems => [...prevItems, data.balloonname]);
+    }
+    setAddingBalloon(false);
+  };
+
   const openBalloonInterface = (balloon: String) => {
     console.log(`Öffne Balloon-Interface für ${balloon}`);
   };
@@ -20,35 +28,55 @@ const Navigationbar = () => {
     setDropdownItems(updatedItems);
   }
 
-    return (
-        <Navbar fluid rounded>
-          <Navbar.Brand href="http://localhost:3000/">
-            <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Balloon Logo" />
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">IoT Wetterballon</span>
-          </Navbar.Brand>
-          <div className="flex md:order-2">
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={"Ballons"}
-            >
-            <ul>
-              {dropdownItems.map((item, index) => (
-                <li key={index}>
-                  <Dropdown.Item>
-                    <button className="ml-1" onClick={() => openBalloonInterface(item)}>{item}</button>
-                    <button className="ml-3" onClick={() => removeBalloonInterface(item)}>X</button>
-                  </Dropdown.Item>
-                </li>
-              ))}
-            </ul>
-              <Dropdown.Divider />
-              <Dropdown.Item><button onClick={addDropdownItem}>Add Ballon</button></Dropdown.Item>
-            </Dropdown>
-            <Navbar.Toggle />
-          </div>
-        </Navbar>
-      );
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="http://localhost:3000/">
+        <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Balloon Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">IoT Wetterballon</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={"Ballons"}
+          dismissOnClick={false}
+        >
+          {dropdownItems.map((item, index) => (
+            <Dropdown.Item as="div" onClick={() => openBalloonInterface(item)} key={index}>
+              {item}
+              <div><button className="ml-3" onClick={() => removeBalloonInterface(item)}>X</button></div>
+            </Dropdown.Item>
+          ))}
+          <Dropdown.Divider />
+          <Dropdown.Item as="div">
+            {addingBalloon ? (
+              <HouseForm onSubmit={addBalloon}>
+                {({ submit }) => (
+                  <form onSubmit={submit}>
+                    <Field name="balloonname" initialValue={""}>
+                      {({ value, setValue, onBlur }) => (
+                        <input
+                          value={value}
+                          onChange={(e) => setValue(e.currentTarget.value)}
+                          onBlur={onBlur}
+                          placeholder="name"
+                        />
+                      )}
+                    </Field>
+                    <button type="submit">+</button>
+                  </form>
+                )}
+              </HouseForm>
+            ) : (
+              <button onClick={setAddingBalloonTrue}>Add Ballon</button>
+            )}
+          </Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+    </Navbar>
+
+  );
 }
 
 export default Navigationbar;
