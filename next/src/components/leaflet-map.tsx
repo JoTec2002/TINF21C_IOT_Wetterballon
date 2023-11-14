0a2192b3e3c3d0ac049d048ace12a56ed9953656
ro5zi7 <robin.ziegler@it-designers.de>
2023-11-14 15:52:26 +0100

    automatic creation of markers and connecting line between them from given array

--- next/src/components/leaflet-map.tsx
+++ next/src/components/leaflet-map.tsx
@@ -1,23 +1,30 @@
 'use client'
-import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
-import dynamic from "next/dynamic";
+import {MapContainer, Marker, Polyline, Popup, TileLayer} from 'react-leaflet'
 
 
-const centerCoords: any = [48.7758, 9.1829]
+const locations = [
+    {name: "CENTER", lat: 48.7758, lng:  9.1829},
+    {name: "LOCATION_1", lat: 48.8312, lng: 10.1829},
+    {name: "LOCATION_2", lat: 48.9804, lng: 11.1829},
+];
 
 const LeafletMap = () => {
     return (
-
-            <MapContainer id="map" center={centerCoords} zoom={10} scrollWheelZoom={false}>
+            <MapContainer id="map" center={[locations[0].lat, locations[0].lng]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={centerCoords}>
-                    <Popup>
-                        Stuttgart Mitte
-                    </Popup>
-                </Marker>
+                {locations.map((location, index) => (
+                    <Marker key={index} position={[location.lat, location.lng]}>
+                        <Popup><b>{location.name}</b></Popup>
+                    </Marker>
+                ))}
+                <Polyline positions={locations.map(location => [location.lat, location.lng])} />
+
+
+
+
             </MapContainer>
 
         )
