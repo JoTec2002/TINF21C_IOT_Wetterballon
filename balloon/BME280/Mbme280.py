94675d6bcbb50cbbac3a15ac983055b8947f3a4d
Jonas Graubner <82174634+JoTec2002@users.noreply.github.com>
2023-11-22 20:26:21 +0100

    Catch Communication Errors with BME280

--- balloon/BME280/Mbme280.py
+++ balloon/BME280/Mbme280.py
@@ -1,11 +1,15 @@
 import bme280
+from loguru import logger
+
 
 class Mbme280:
     def __init__(self, bus, address):
         self.bus = bus
         self.address = address
-        self.calibration_params = bme280.load_calibration_params(bus, address)
-        pass
+        try:
+            self.calibration_params = bme280.load_calibration_params(bus, address)
+        except OSError:
+            logger.warning("BME 280 not reachable")
 
     def read_temp_pressure_humidity(self):
         data = bme280.sample(self.bus, self.address, self.calibration_params)
